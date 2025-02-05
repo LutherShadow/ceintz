a3edebef23660e4aa6e24ee231df8be72de04b8f
LutherShadow <joseantoniomercadosantiago9@gmail.com>
2025-02-04 21:41:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 <?php
 // Redirigir a login.php
 header("Location: login.php");
 exit();
 ?>
-=======
-<?php
-// Redirigir a login.php
-header("Location: login.php");
-exit();
-?>
->>>>>>> 94c21dd0d80bee8c8120cd1b0e1fd16bb3bc905b
