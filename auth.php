1a179e04c1ea8fabf1019786ad501d684d3d4b38
LutherShadow <joseantoniomercadosantiago9@gmail.com>
2025-02-04 21:37:49 -0600

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 if (!isset($_SESSION['user_id'])) {
@@ -6,13 +5,4 @@ if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
     exit();
 }
-=======
-<?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    // Redirigir al inicio de sesión si no hay una sesión activa
-    header('Location: login.php');
-    exit();
-}
->>>>>>> 94c21dd0d80bee8c8120cd1b0e1fd16bb3bc905b
 ?>
